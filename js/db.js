1c7166c21ac6c18f5f8eff0d5f588933db4c4bbf
Saurabh Tiwari <11601712+saurabh135@users.noreply.github.com>
2019-01-17 11:20:39 +0530

    Add files via upload

---
+++ js/db.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyCYVtReuEIXy35K-0nOzbXHv3XpcTQ2XaI",  //api key of proj. created from webiste
+    authDomain: "temp-dab3c.firebaseapp.com",
+    databaseURL: "https://temp-dab3c.firebaseio.com",
+    projectId: "temp-dab3c",
+    storageBucket: "",
+    messagingSenderId: "232615491034"
+  };
+  firebase.initializeApp(config);   
